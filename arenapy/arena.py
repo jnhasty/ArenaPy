2db943498794b3fedf0060c8168cebc590179d9e
jnhasty <jnhasty@gmail.com>
2012-07-25 12:38:54 -0400

    -better documentation -better handling of data (key checking, etc)

--- arenapy/arena.py
+++ arenapy/arena.py
@@ -13,63 +13,104 @@ class ArenaPy(object):
     def __init__(self):
         self.base_url = BASE_URL
 
-    def make_request(self, url_frag):
+    def make_request(self, url_frag, options={}):
         '''
         + core request function
         + requires a url fragment
         + returns json data as a dictionary
         '''
-        request = requests.get('%s%s' % (self.base_url, url_frag))
+        request = requests.get('%s%s' % (self.base_url, url_frag), params=options)
         #watch out for bad requests...
         request.raise_for_status()
         return request.json
-
-    ## Basic object requests
-    def get_channel(self, channel, options={}):   
+    
+    #######
+    ## Basic object requests, all return dictionaries
+    #######
+    def get_channel(self, channel):   
+        '''
+        + returns a channel dict:
+            username, channels, slug, user_id, memberships,
+            blocks_count, title, type, created_at, last_block_updated_at,
+            updated_at, collaboration, channel_type, avatar, published,
+            blocks, open, id, user_url
+        
+        + no params available
+        '''
         return self.make_request('/channels/%s' % channel) 
 
+    def get_blocks_for_channel(self, channel, options={}):
+        '''
+        + returns a dictionary: blocks, page, per, channel
+        
+        + optional params are:
+            depth: Defaults to public. Passing extended returns the available connections on a block
+            page: Specifies the page of results to retrieve.
+            per: Specifies the number of channels to retrieve.
+        '''
+        return self.make_request('/blocks?channel=%s' % channel, options)
+
     def get_user_channel(self, id, options={}):
-        return self.make_request('/channels?user=%s' % id)
+        '''
+        + returns a dictionary: blocks, page, per, user
+
+        + optional params are:
+            page: Specifies the page of results to retrieve.
+            per: Specifies the number of channels to retrieve.
+        '''
+        return self.make_request('/channels?user=%s' % id, options)
 
     def get_block(self, id, options={}):
-        return self.make_request('/blocks/%s' % id)
+        '''
+        + returns a block dict:
+            provider_url,image_file_size, embed_source_url, updated_at, 
+            embed_height, image_remote_url, block_type, id, user_id, 
+            image_updated_at, title, comment_count, content, state, 
+            image_thumb, provider_name, type, embed_width, username, 
+            readable_updated_at, image_content_type, embed_url, description,
+            embed_author_url, processing, embed_thumbnail_url, image_original,
+            image_file_name, embed_html, embed_author_name, source_url, 
+            image_display, user_slug, block_class, created_at, generated_title, 
+            remote_source_url, embed_type, link_url, embed_title
 
-    def get_blocks_for_channel(self, channel, options={}):
-        return self.make_request('/blocks?channel=%s' % channel)
+        + optional params are:
+            depth: Defaults to public. Passing extended returns the available connections on a block
+        '''
+        return self.make_request('/blocks/%s' % id, options)
 
     #######
-    ## SORTING, RETRIEVING FUNCTIONS
+    ## SORTING & ITEM RETRIEVAL FUNCTIONS
     #######
 
     #
     # channel basics
     #
     def get_channel_title(self, channel):
-        return channel['title']
+        return channel.get('title')
 
-    
     #
     # channel channels
     #
     def get_channel_channels(self, channel):
-        return channel['channels']
+        return channel.get('channels')
 
     def get_channel_channels_count(self, channel):
-        return len(self.get_channel_channels(channel))
+        if self.get_channel_channels(channel):
+            return len(self.get_channel_channels(channel))
 
     #
     # channel blocks
     #
     def get_channel_block_count(self, channel):
-        return channel['blocks_count']
+        return channel.get('blocks_count')
 
     def sort_channel_blocks_by_created(self, channel, sort_by = 'desc'):
         '''
         sort a channel's blocks by created timestamp
         sort_by asc or desc, default = desc (newest)
         '''
-        if channel.get('blocks'):
-            blocks = channel['blocks']
+        blocks = channel.get('blocks')
+        if blocks:
             if sort_by == 'asc':
                 blocks = sorted(blocks, key=itemgetter('created_at')) 
             else:
@@ -79,54 +120,59 @@ class ArenaPy(object):
         else:
             return None
 
-    def get_channel_images(self, channel):
-        if channel.get('blocks'):
-            return [block for block in channel['blocks'] if block['block_class'] == 'image']
+    #
+    #  block filtering: all take a list of blocks
+    #
+    def get_image_blocks(self, blocks_list):
+        if blocks_list:
+            return [block for block in blocks_list if block['block_class'] == 'image']
         else:
             return None
 
-    def get_channel_images_count(self, channel):
-        if channel.get('blocks'):
+    def get_image_blocks_count(self, blocks_list):
+        if self.get_channel_images(channel):
             return len(self.get_channel_images(channel))
         else:
             return None
 
-    def get_channel_media(self, channel):
-        if channel.get('blocks'):
-            return [block for block in channel['blocks'] if block['block_class'] == 'media']
+    def get_media_blocks(self, blocks_list):
+        if blocks_list:
+            for b in blocks_list:
+                print b
+                print '+++++'
+            return [block for block in blocks_list if block['block_class'] == 'media']
         else:
             return None
 
-    def get_channel_media_count(self, channel):
-        if channel.get('blocks'):
+    def get_media_blocks_count(self, blocks_list):
+        if self.get_channel_media(channel):
             return len(self.get_channel_media(channel))
         else:
             return None
 
-    def get_channel_text(self, channel):
-        if channel.get('blocks'):
-            return [block for block in channel['blocks'] if block['block_class'] == 'text']
+    def get_text_blocks(self, blocks_list):
+        if blocks_list:
+            return [block for block in blocks_list if block['block_class'] == 'text']
         else:
             return None
 
-    def get_channel_text_count(self, channel):
-        if channel.get('blocks'):
+    def get_text_blocks_count(self, blocks_list):
+        if self.get_channel_text(channel):
             return len(self.get_channel_text(channel))
         else:
             return None
 
-    def get_channel_links(self, channel):
-        if channel.get('blocks'):
-            return [block for block in channel['blocks'] if block['block_class'] == 'link']
+    def get_links_blocks(self, blocks_list):
+        if blocks_list:
+            return [block for block in blocks_list if block['block_class'] == 'link']
         else:
-            return None
+        +    return None
 
-    def get_channel_links(self, channel):
-        if channel.get('blocks'):
+    def get_links_blocks_count(self, blocks_list):
+        if self.get_channel_text(blocks_list):
             return len(self.get_channel_text(channel))
         else:
             return None
-
 
     #
     # channel connections
@@ -150,4 +196,7 @@ class ArenaPy(object):
         '''
         returns count of unique connections
         '''
-        return len(self.get_channel_connections(channel))
+        if self.get_channel_connections(channel):
+            return len(self.get_channel_connections(channel))
+        else:
+            return None
